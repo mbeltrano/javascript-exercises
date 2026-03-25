7e74b09c4ec91cfe57e15ad4e5546190f546aa26
Mattia Beltrano <mattia.beltrano@external.a2a.it>
2026-03-25 22:15:05 +0100

    completed calculator

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,24 +1,47 @@
-const add = function() {
+const add = function(num1,num2) {
+  if(isNaN(num1) || isNaN(num2)) return;
+	return num1 + num2;
+};
+
+const subtract = function(num1,num2) {
+    if(isNaN(num1) || isNaN(num2)) return;
+    return num1 - num2;
 	
 };
 
-const subtract = function() {
+const sum = function(array) {
+  let result = 0;
+   // if(isNaN(num1) || isNaN(num2)) return;
+    for(let ar of array){
+    result += ar;
+  }
+  return result;
 	
 };
 
-const sum = function() {
+const multiply = function(array) {
+  let result = 1; 
+  // if(isNaN(num1) || isNaN(num2)) return;
+  for(let ar of array){
+    result *= ar;
+  }
+  return result;
+
+};
+
+const power = function(num1,num2) {
+    if(isNaN(num1) || isNaN(num2)) return;
+return num1**num2;
 	
 };
 
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
+const factorial = function(num1) {
+      if(isNaN(num1)) return;
+      let fact = 1;
+    for (let i =1; i<=num1; i++){
+      fact *=i;
+    }
+    return fact;
 	
 };
 
