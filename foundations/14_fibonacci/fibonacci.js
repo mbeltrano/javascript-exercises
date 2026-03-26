8f147e0c90f030e67107a2644a59b0d4e35a14e9
Mattia Beltrano <mattia.beltrano@external.a2a.it>
2026-03-26 21:54:49 +0100

    finished 14 fibonacci

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,4 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+    if (typeof num !== 'number') {
+        num = parseInt(num)
+    } else num;
+    if (num < 0) return "OOPS";
+    if (num === 0) return 0;
+    let tempNum1;
+    let tempNum3 = [1, 1];
+    if (num > 2) {
+        for (let i = 0; i < num - 2; i++) {
+            tempNum1 = tempNum3[i] + tempNum3[i + 1];
+            tempNum3.push(tempNum1);
+        }
+    }
+    console.log(tempNum3[num - 1]);
+    return tempNum3[num - 1];
 
 };
 
