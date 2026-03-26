304d9829dde8027b4eed39ff36beb85e21acac05
Mattia Beltrano <mattia.beltrano@external.a2a.it>
2026-03-26 21:29:17 +0100

    finished palindromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,4 +1,16 @@
-const palindromes = function () {
+const palindromes = function (stringa) {
+    const str = stringa
+                    .replace(/[^\w\s\']|_/g, "")
+                    .replace(/\s+/g, "")
+                    .toUpperCase();
+    console.log(str);
+    const reverse = str
+                      .split("")
+                      .reverse()
+                      .join("");
+    console.log(reverse);
+    console.log(str === reverse);
+    return str === reverse;
 
 };
 
